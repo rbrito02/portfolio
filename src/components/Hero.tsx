bc0b9bb66d0ad7b168ca9cb0565a458fc60f4e63
Raudy Brito <raudyb02@gmail.com>
2024-01-08 13:33:26 -0500

    Finished Scrolling Subheader

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,18 +4,28 @@ import "./styles/scroll.css";
 
 const Hero = () => {
   return (
-    <div className="container flex gap-2">
-      <span className="text-[#5f6368] text-3xl font-bold h-[3rem]">
+    <div className="container flex gap-3 items-center justify-center ">
+      <span className="h-28 text-[#5f6368] text-3xl font-bold flex items-center">
         I&apos;m A
       </span>
-      <div className="scrolling-words-container">
-        <div className="scrolling-words-box">
-          <ul>
-            <li className="text-[#ea4335]">Devloper</li>
-            <li className="text-[#4285f4]">Student</li>
-            <li className="text-[#34a853]">Fullstack Engineer</li>
-            <li className="text-[#fbbc04]">Musician</li>
-            <li className="text-[#ea4335]">Developer</li>
+      <div className="scrolling-words-container flex items-center font-4xl font-bold">
+        <div className="scrolling-words-box h-28 m-auto overflow-hidden text-3xl">
+          <ul className="mx-0 p-0">
+            <li className="flex items-center justify-start h-28 list-none text-[#ea4335]">
+              Developer
+            </li>
+            <li className="flex items-center justify-start h-28 list-none text-[#4285f4]">
+              Student
+            </li>
+            <li className="flex items-center justify-start h-28 list-none text-[#34a853]">
+              Fullstack Engineer
+            </li>
+            <li className="flex items-center justify-start h-28 list-none text-[#fbbc04]">
+              Musician
+            </li>
+            <li className="flex items-center justify-start h-28 list-none text-[#ea4335]">
+              Developer
+            </li>
           </ul>
         </div>
       </div>
